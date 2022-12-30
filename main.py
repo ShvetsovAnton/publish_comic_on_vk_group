3611e75f8dd002ed85bdb465b2d102888db7df05
Anton <56098143+ShvetsovAnton@users.noreply.github.com>
2022-12-30 13:18:14 +0600

    Изменил логику
    В файле `main.py` поправил импорты.
    В функции `main`удалил неиспользуемые переменные.

--- main.py
+++ main.py
@@ -1,38 +1,13 @@
 import os
-import random
 
-import requests
 from dotenv import load_dotenv
 
-from down_load_tools import delete_published_comic
-from down_load_tools import download_image_in_folder
-from down_load_tools import take_file_name_from_url
+from down_load_tools import download_random_comic
+from down_load_tools import delete_comic
 from vk_load_tools import get_server_url
 from vk_load_tools import post_comic
 from vk_load_tools import save_comic_in_album
 from vk_load_tools import upload_comic_on_server
-
-
-def get_total_number_comics():
-    url = "https://xkcd.com/info.0.json"
-    response = requests.get(url)
-    return response.json()["num"]
-
-
-def get_comic_comment(comic_number):
-    url = f"https://xkcd.com/{comic_number}/info.0.json"
-    response = requests.get(url)
-    response.raise_for_status()
-    comic_description = response.json()
-    return comic_description["alt"]
-
-
-def get_comic_url(comic_number):
-    url = f"https://xkcd.com/{comic_number}/info.0.json"
-    response = requests.get(url)
-    response.raise_for_status()
-    comic_description = response.json()
-    return comic_description["alt"]
 
 
 def main():
@@ -40,13 +15,9 @@ def main():
     vk_access_token = os.environ["VK_ACCESS_TOKEN"]
     group_id = int(os.environ["VK_GROUP_ID"])
     vk_api_version = 5.131
-    comics_total_number = get_total_number_comics()
-    first_comic = 1
-    comic_number = random.randint(first_comic, comics_total_number)
-    comic_comment = get_comic_comment(comic_number)
-    comic_url = get_comic_url(comic_number)
-    file_name = take_file_name_from_url(comic_url)
-    download_image_in_folder(comic_url, file_name)
+    file_name_and_comic_comment = download_random_comic()
+    file_name = file_name_and_comic_comment["file_name"]
+    comic_comment = file_name_and_comic_comment["comic_comment"]
     try:
         upload_url = get_server_url(
             vk_access_token,
@@ -83,7 +54,7 @@ def main():
             vk_api_version
         )
     finally:
-        delete_published_comic(file_name)
+        delete_comic(file_name)
 
 
 if __name__ == "__main__":
